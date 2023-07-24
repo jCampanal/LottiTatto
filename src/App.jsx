3ffccdb13206a5458e0055ffb341d43e1058a79f
J Campanal <jcampanal08@gmail.com>
2023-07-24 11:54:54 -0400

    fixed error build -change r for R in app.jsx-

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import  Menu  from './Components/Menu/Menu'
 import { Logo } from './assets/Photos/Photos'
 import './App.css'
-import AppRoutes from './routes/routes'
+import AppRoutes from './routes/Routes'
 import PictureView from './Components/PictureView/PictureView'
 //#endregion
 
